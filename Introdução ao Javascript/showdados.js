ee444c9eb7cf930f51143de31ac3ed4c02bd8c0b
wellingotn-brito <berews02@gmail.com>
2021-12-08 16:32:12 -0300

    javascript intro

---
+++ Introdução ao Javascript/showdados.js
@@ -0,0 +1,12 @@
+/* testando github copilot */
+//do a function that returns data from array of 0 to 10 in a console.log
+function showDados() {
+    var dados = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (var i = 0; i < dados.length; i++) {
+        console.log(dados[i]);
+    }
+}
+
+/* testando github copilot */
+//call the function showDados
+showDados();
